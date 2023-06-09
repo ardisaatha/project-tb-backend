ad2afd05b0adbac27b0aeddbdd1704c504975cd9
Atha Ardisa <ardisaatha1@gmail.com>
2023-06-09 22:42:01 +0700

    add routes for location

--- config/routes/index.js
+++ config/routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const auth = require("./auth");
+const loc = require("./loc")
 // API
 router.use("/api/auth/", auth);
+router.use("/api/location", loc)
 
 module.exports = router;
