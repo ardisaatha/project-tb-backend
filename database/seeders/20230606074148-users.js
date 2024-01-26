8286781217827bd0a12eb6ae838e35e9ddcf13a7
ardisaatha <92588716+ardisaatha@users.noreply.github.com>
2024-01-26 17:18:37 +0700

    new seed

--- database/seeders/20230606074148-users.js
+++ database/seeders/20230606074148-users.js
@@ -6,9 +6,27 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert("users", [
       {
+        id: 1,
+        username: "Admin",
+        password: bcrypt.hashSync("#admin123", 10),
+        role: "admin",
+        wilayah: "admin",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 2,
+        username: "DemoTawangsari",
+        password: bcrypt.hashSync("demouser", 10),
+        role: "user",
+        wilayah: "Tawangsari",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
         id: 3,
-        username: "admin2",
-        password: bcrypt.hashSync("#admin13", 10),
+        username: "admin",
+        password: bcrypt.hashSync("$admin32", 10),
         role: "admin",
         wilayah: "admin",
         createdAt: new Date(),
