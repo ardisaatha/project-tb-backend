ad95e6b5e0a6fb481dda76c8607a2424881c4b8b
Ahmad Fajar Kusumajati <fajarku26@gmail.com>
2024-01-26 17:50:16 +0700

    seed

--- database/seeders/20230606074148-users.js
+++ database/seeders/20230606074148-users.js
@@ -5,27 +5,27 @@ const bcrypt = require("bcrypt");
 module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert("users", [
-      {
-        id: 1,
-        username: "Admin",
-        password: bcrypt.hashSync("#admin123", 10),
-        role: "admin",
-        wilayah: "admin",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        id: 2,
-        username: "DemoTawangsari",
-        password: bcrypt.hashSync("demouser", 10),
-        role: "user",
-        wilayah: "Tawangsari",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
+      // {
+      //   id: 1,
+      //   username: "Admin",
+      //   password: bcrypt.hashSync("#admin123", 10),
+      //   role: "admin",
+      //   wilayah: "admin",
+      //   createdAt: new Date(),
+      //   updatedAt: new Date(),
+      // },
+      // {
+      //   id: 2,
+      //   username: "DemoTawangsari",
+      //   password: bcrypt.hashSync("demouser", 10),
+      //   role: "user",
+      //   wilayah: "Tawangsari",
+      //   createdAt: new Date(),
+      //   updatedAt: new Date(),
+      // },
       {
         id: 4,
-        username: "admin",
+        username: "admin2",
         password: bcrypt.hashSync("$admin32", 10),
         role: "admin",
         wilayah: "admin",
